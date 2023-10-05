ddb345d8fa2fab4c09d49d29039e7ae97b949f16
Oscardk <dauren2050@yandex.ru>
2023-10-05 22:31:24 +0600

    go_host_v2

--- cmd/main.go
+++ cmd/main.go
@@ -2,27 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 	"urlshortener/pkg"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
 }
 
-func (u User) getAllinfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.name, u.age, u.money)
+func (u User) GetAllinfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money equal: %d", u.Name, u.Age, u.Money)
 }
 
-func (u *User) setNewName(newName string) {
-	u.name = newName
+func (u *User) SetNewName(newName string) {
+	u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"Bob", 25, -50, 4.2, 0.8}
+	tmpl, _ := template.ParseFiles("templates/home_page.html") // Используйте правильное имя файла
+	tmpl.Execute(w, bob)
+
 	if r.Method == http.MethodPost {
 		body, err := ioutil.ReadAll(r.Body)
 		defer r.Body.Close()
@@ -41,11 +46,9 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	bob := User{"Bob", 25, -50, 4.2, 0.8}
-	bob.setNewName("Alexander")
-
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintf(w, bob.getAllinfo())
+	//bob.SetNewName("Alexander")
+	//w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	//fmt.Fprintf(w, bob.GetAllinfo())
 }
 
 func contact_page(w http.ResponseWriter, r *http.Request) {
