eaca500c154ff890259d8053d4c7481efe27a705
Oscardk <dauren2050@yandex.ru>
2023-10-05 16:23:57 +0600

    go_host

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"urlshortener/pkg"
 )
 
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the home page, GO is power!")
+}
+
+func contact_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go is good!!!")
+}
+
 func main() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts", contact_page)
+	http.ListenAndServe(":8080", nil)
 	us := pkg.New()
 	shortUrl := us.Encode("http://hdrezka.me/films/adventures/57217-gran-turizmo-2023.html")
 	fmt.Println("Short URL:", shortUrl)
 
 	longUrl, _ := us.Decode(shortUrl)
 	fmt.Println("Long URL:", longUrl)
+
 }
